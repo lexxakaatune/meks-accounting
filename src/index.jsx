cc7fd4dd959c840c35ec26aeea5acfb6a23af3fb
Casimia Alex <alexcasimia@gmail.com>
2026-01-05 15:18:01 +0100

    back to browserRouter

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/css/index.css';
 import App from './App';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
